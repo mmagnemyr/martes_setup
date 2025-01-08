1409e293276ec0f77fb8827cef80ee254bac75f5
Martin Magnemyr <martin.magnemyr@gmail.com>
2025-01-08 21:15:03 +0100

    volume form mongofb set correct

--- docker_mongo_restore.sh
+++
@@ -1,4 +0,0 @@
-docker exec martes_mongodb rm -rf /backup
-docker cp ./backup martes_mongodb:/restore_backup3
-docker exec -i martes_mongodb mongorestore   --username admin   --password secret   --authenticationDatabase admin   --drop   --dir /restore_backup3
-
