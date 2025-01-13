7ac90f6718fb894eecf1151d09b2e38c57a13f49
Martin Magnemyr <martin.magnemyr@gmail.com>
2025-01-13 21:13:44 +0100

    updated backup.sh

---
+++ backup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+  echo "Usage: $0 <windows_path>"
+  exit 1
+fi
+
+WIN_PATH=$1
+LINUX_PATH=$(echo $WIN_PATH | sed 's|\\|/|g' | sed 's|C:|/mnt/c|')
+
+rm -rf backup
+mkdir backup
+cp -r $LINUX_PATH/* ./backup/
+
+
+docker exec martes_mongodb rm -rf /backup
+docker exec -i martes_mongodb mongodump --username admin --password secret --authenticationDatabase admin --out /backup
+docker cp martes_mongodb:/backup ./mongo_backup
+
+# Copy the backup to the Windows path + /backup
+WIN_BACKUP_PATH=$(echo $WIN_PATH | sed 's|\\|/|g' | sed 's|C:|/mnt/c|')/backup
+mkdir -p $WIN_BACKUP_PATH
+cp -r ./mongo_backup/* $WIN_BACKUP_PATH/
