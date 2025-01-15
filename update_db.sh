43e68f946dc456bf1cbf36bd08b4e273eaa8ef23
magnemyr <martin.magnemyr@gmail.com>
2025-01-15 11:13:48 +0100

    updated update_db.sh

--- update_db.sh
+++ update_db.sh
@@ -1,4 +1,2 @@
-docker exec martes_backend rm -rf /msaccess
-docker cp ./msaccess martes_backend:/msaccess
 # Execute the import.sh script located in the /app folder of the container
 docker exec martes_backend sh -c "cd /app && python ./macli.py -action init -lk system"
