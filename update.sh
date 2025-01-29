1cf6db2e1a8212c2fda0862773ec45807dbf38af
Martin Magnemyr <martin.magnemyr@gmail.com>
2025-01-29 07:08:03 +0100

    update

--- update.sh
+++ update.sh
@@ -1,6 +1,12 @@
-git pull
+# turn off the running containers
 docker compose down
+
+# pull updates
 docker compose pull
+
+# start conntainers
 docker compose up -d
+
+# remove any unused containers
 docker image prune -f
 
