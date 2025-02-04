0e505dc35a6808f28b91f39e603fe7946f5680aa
Martin Magnemyr <martin.magnemyr@gmail.com>
2025-02-04 07:11:51 +0100

    modified backup.sh

--- backup.sh
+++ backup.sh
@@ -14,10 +14,10 @@ cp -r $LINUX_PATH/* ./backup/
 
 
 docker exec martes_mongodb rm -rf /backup
-docker exec -i martes_mongodb mongodump --username admin --password secret --authenticationDatabase admin --out /backup
-docker cp martes_mongodb:/backup ./mongo_backup
+docker exec -i martes_mongodb mongodump --username admin --password secret --authenticationDatabase admin --out ./mongo_backup
+# docker cp martes_mongodb:/backup ./mongo_backup
 
 # Copy the backup to the Windows path + /backup
-WIN_BACKUP_PATH=$(echo $WIN_PATH | sed 's|\\|/|g' | sed 's|C:|/mnt/c|')
-mkdir -p $WIN_BACKUP_PATH
-cp -r ./mongo_backup/* $WIN_BACKUP_PATH/
+# WIN_BACKUP_PATH=$(echo $WIN_PATH | sed 's|\\|/|g' | sed 's|C:|/mnt/c|')
+# mkdir -p $WIN_BACKUP_PATH
+# cp -r ./mongo_backup/* $WIN_BACKUP_PATH/
